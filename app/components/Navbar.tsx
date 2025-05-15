d43bf387c6c3030c7666a7a50187c6f7bae2c6ae
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-05-15 11:01:15 +1000

    Login with Github is working

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,46 @@
-import { auth, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = async () => {
   const session = await auth();
+
   return (
     <header className="px-5 py-1 bg-[#E8E7E7] shadow-sm">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={120} height={100} />
         </Link>
+
         <div className="flex items-center gap-5 text-black">
-          {session && session?.user ? (
+          {session?.user ? (
             <>
               <Link href="/startup/create">
                 <span>Create</span>
               </Link>
-              <button onClick={signOut}></button>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/user/${session.user.name || session.user.email}`}>
+                <span>{session.user.name?.toUpperCase()}</span>
+              </Link>
             </>
           ) : (
-            <></>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
           )}
         </div>
       </nav>
--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
-// lib/auth.ts or auth.ts
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github"; // or any other provider
+import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+const { auth, handlers, signIn, signOut } = NextAuth({
     providers: [GitHub],
     secret: process.env.NEXTAUTH_SECRET,
 });
-  
+
+export { auth, handlers, signIn, signOut };
+
