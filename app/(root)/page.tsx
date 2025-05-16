be8822a35ab53d36bc7eb2b504ed702a17ac61ca
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-05-16 16:00:06 +1000

    Sanity Search feature is working.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -7,20 +9,12 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Dhananjay" },
-      _id: 1,
-      description: "This is a description.",
-      image:
-        "https://images.unsplash.com/photo-1613923339596-bd8f3ec7abd5?q=80&w=3500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const { data: posts } = await sanityFetch({
+    query: STARTUPS_QUERY,
+    params,
+  });
 
   return (
     <>
@@ -39,12 +33,15 @@ export default async function Home({
         </p>
         <ul className="card_grid mt-7">
           {posts?.length > 0 ? (
-            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
           ) : (
             <p className="no-results">No Jobs Found</p>
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,28 +4,20 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
+import { Startup, Author } from "@/sanity/types";
 
-type StartupCardType = {
-  _createdAt: string;
-  views: number;
-  author: { _id: number; name: string };
-  _id: number;
-  description: string;
-  image: string;
-  category: string;
-  title: string;
-};
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
-const StartupCard = ({ post }: { post: StartupCardType }) => {
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
     _createdAt,
     views,
     author,
-    _id,
-    description,
-    image,
-    category,
     title,
+    category,
+    _id,
+    image,
+    description,
   } = post;
 
   return (
@@ -41,39 +33,46 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
       </div>
       <div className="flex justify-between items-center mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${author._id}`}>
+          <Link href={`/user/${author?._id ?? ""}`}>
             <p className="font-medium text-[18px] text-[#0D1B2A] line-clamp-1">
-              {author?.name}
+              {author?.name ?? "Anonymous"}
             </p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${author._id}`}>
-          <Image
-            src="https://images.unsplash.com/photo-1687360440741-f5df549b352d?q=80&w=3351&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt={`${author.name}'s profile image`}
-            width={48}
-            height={48}
-            className="rounded-full object-cover"
-          />
-        </Link>
+        {author?._id && (
+          <Link href={`/user/${author._id}`}>
+            <Image
+              src="https://images.unsplash.com/photo-1687360440741-f5df549b352d?q=80&w=3351&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt={`${author?.name}'s profile image`}
+              width={48}
+              height={48}
+              className="rounded-full object-cover"
+            />
+          </Link>
+        )}
       </div>
       <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
-        <Image
-          src={image}
-          alt="placeholder"
-          width={100}
-          height={100}
-          className="startup-card_img"
-        />
+        {image && (
+          <Image
+            src={image}
+            alt="Startup image"
+            width={200}
+            height={200}
+            className="startup-card_img"
+          />
+        )}
       </Link>
       <div className="flex justify-between items-center gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
-          <p className="text-16-medium">{category}</p>
-        </Link>
+        {category && (
+          <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className="text-16-medium">{category}</p>
+          </Link>
+        )}
+
         <Button className="startup-card_btn" asChild>
           <Link href={`/startup/${_id}`}>Details</Link>
         </Button>
